57ba371a6a1b3587f30fd2dd570bb72fca5aa474
Bakhromjon Sadullaev <bakhromjon_sadullaev@epam.com>
2022-08-30 17:53:30 +0500

    fix: code style

--- src/App/components/Tools/Tools.jsx
+++ src/App/components/Tools/Tools.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import style from './Tools.module.scss'
-import toolsImg from '../../../assets/images/Softwares.png'
-import Headline from '../../../assets/images/Headline.png'
+import React from "react";
+import style from "./Tools.module.scss";
+import toolsImg from "../../../assets/images/Softwares.png";
 
 function Tools() {
   return (
-    <div className={style.wrapper} >
-      <div className={style.container} >
+    <div className={style.wrapper}>
+      <div className={style.container}>
         <h1>Tools We Use For The Best Learning Experience</h1>
-      <img src={toolsImg} alt="tools image" className={style.toolsImg}  />
-</div>
-</div>
-
-  )
+        <img src={toolsImg} alt="tools" className={style.toolsImg} />
+      </div>
+    </div>
+  );
 }
 
-export default Tools
+export default Tools;
