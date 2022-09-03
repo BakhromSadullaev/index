04e3ce9a26d3d309ffc4bb5cfce39e2543ab2d68
KABIR <kabir_karamatjanov@mail.ru>
2022-09-03 17:55:40 +0500

    fat: footer done

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import PlatformSection from "./components/PlatformSection/PlatformSection";
 import Tools from "./components/Tools/Tools";
 import TrackSection from "./components/TrackSection/TrackSection";
 import Teachers from './components/Teachers/Teachers'
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Tools/>
       <Teachers />
       <Parents/>
+      <Footer/>
     </div>
   );
 }
