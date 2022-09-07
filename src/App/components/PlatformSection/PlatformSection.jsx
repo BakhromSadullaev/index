538fafad241d40a8a0c1f1b6a1ae6d7186b57d26
KABIR <kabir_karamatjanov@mail.ru>
2022-09-07 06:17:15 +0500

    border removed

--- src/App/components/PlatformSection/PlatformSection.jsx
+++ src/App/components/PlatformSection/PlatformSection.jsx
@@ -18,11 +18,11 @@ const PlatformSection = () => {
           </article>
           <article>
             <div className={style.block}></div>
-            <p>Customized Learning</p>
+            <p>Award Winning Curriculum</p>
           </article>
           <article>
             <div className={style.block}></div>
-            <p>Customized Learning</p>
+            <p>100% Satisfaction Guaranteed</p>
           </article>
 
           <div className={style.infoBlock}>
@@ -32,18 +32,18 @@ const PlatformSection = () => {
             </article>
 
             <article>
-              <h1>1300</h1>
-              <p>Registrations</p>
+              <h1>11</h1>
+              <p>Countries</p>
             </article>
 
             <article>
-              <h1>1300</h1>
-              <p>Registrations</p>
+              <h1>91%</h1>
+              <p>Student Retention</p>
             </article>
 
             <article>
-              <h1>1300</h1>
-              <p>Registrations</p>
+              <h1>5*</h1>
+              <p>Rating</p>
             </article>
           </div>
         </div>
