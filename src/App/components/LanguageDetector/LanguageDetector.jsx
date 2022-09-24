900f02e1998926e96133b5fe2c21e265fc34b8d0
Bakhromjon Sadullaev <bakhromjon_sadullaev@epam.com>
2022-09-24 17:24:49 +0500

    fix: slider corrected

--- src/App/components/LanguageDetector/LanguageDetector.jsx
+++ src/App/components/LanguageDetector/LanguageDetector.jsx
@@ -5,28 +5,22 @@ const LanguageDetector = () => {
   const [lang, setLang] = useState("En");
   const [contextRu, setContextRu] = useState("Ru");
 
-  const handleRu = () => {
-    if (lang === "En") {
-      setContextRu("En");
-    } else if (lang === "Uz") {
-      setContextRu("Uz");
-    }
-    if (lang !== "Ru") setLang("Ru");
+  const handleRu = (e) => {
+    console.log(e.target.textContent);
+    setContextRu(lang);
+    setLang(e.target.textContent);
   };
 
   return (
     <div className={style.container}>
-      <h1 onClick={() => setLang(lang)} className={style.lang}>
+      <h1 onClick={handleRu} className={style.lang}>
         {lang}
       </h1>
       <h1 onClick={handleRu} className={style.lang}>
         {contextRu}
       </h1>
-      <h1
-        onClick={() => setLang(lang !== "Uz" ? "Uz" : "Ru")}
-        className={style.lang}
-      >
-        {lang !== "Uz" ? "Uz" : "Ru"}
+      <h1 onClick={handleRu} className={style.lang}>
+        {contextRu}
       </h1>
     </div>
   );
